440b2f02858bb4bf4c15c2b4b0d8b9aa6d6fcce5
Toby Boudreaux <toby.boudreaux11@gmail.com>
2020-05-07 19:32:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -31,6 +31,6 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   find_valid_calls.find {
     |element|
-    element = valid_calls
+    element = valid_calls.map
   }
 end
