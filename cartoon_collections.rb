906dc70359de922e50e724a332fcc30438913345
Toby Boudreaux <toby.boudreaux11@gmail.com>
2020-05-07 19:27:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,10 @@ end
 def long_planeteer_calls(planeteer_calls)
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
-  array
+  array.find_all {
+    |element|
+    element
+  }
 end
 
 def find_valid_calls(planeteer_calls)
